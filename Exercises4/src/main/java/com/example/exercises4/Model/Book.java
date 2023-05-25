5b1293740d4a3e77d1223c592d72ec901578b4a1
Abdulmajeed <132199458+mj95d@users.noreply.github.com>
2023-05-25 17:44:55 +0300

    Book.java

--- Exercises4/src/main/java/com/example/exercises4/Model/Book.java
+++ Exercises4/src/main/java/com/example/exercises4/Model/Book.java
@@ -6,23 +6,27 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Locale;
-@Data
+@AllArgsConstructor
 @Entity
+@Data
+@NoArgsConstructor
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 @NotNull
+     @Column
     private String title;
 @Column
     private String author;
-
+ @Column
     @Enumerated(EnumType.STRING)
     private Locale.Category category;
 @NotNull
+     @Column
     private Integer ISBN;
-
+ @Column
     @Min(value = 8, message = "Number of least 8")
     private Integer numberOfPages;
 
